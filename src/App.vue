22973922b03a80569370e4a95444af027fb34a82
lyoto07 <guilherme-lira@outlook.com.br>
2022-10-09 21:02:17 -0300

    desenvolvimento do alura tracker para para aprendizado de vue.js

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral/>
+    </div>
+    <div class="column is-three-quarter">
+      <barra-comandos/>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import BarraComandos from './components/BarraComandos.vue';
+export default defineComponent({
+  name: 'App',
+  components:{
+    BarraLateral,
+    BarraComandos
+  }
+  
+});
+</script>
+
+<style>
+
+</style>
